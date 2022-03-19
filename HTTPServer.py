cb3bd070a680f0bbfe0b8bf86f82f5237662adc3
samchenghowing <56993697+samchenghowing@users.noreply.github.com>
2022-03-19 17:45:52 +0800

    Changed get header method

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python
 # https://www.tutorialspoint.com/python/python_multithreading.htm
 # https://stackoverflow.com/questions/17453212/multi-threaded-tcp-server-in-python
 from socket import *
-from datetime import date
+from datetime import date, datetime
 import threading, logging, os
 
 class clientThread(threading.Thread):
@@ -20,44 +19,96 @@ class clientThread(threading.Thread):
     def run(self):
         """Overriding the thread run funtion, this fuction will get the client's request and response"""
         
-        clientRequestStr = self.socket.recv(1024)
+        clientRequestStr = self.socket.recv(1024).decode()
         logging.getLogger('ThreadingHTTPServer').info("clientRequestStr is:\n" + clientRequestStr)
 
+        headers = clientRequestStr.split("\n")
+        fields = headers[0].split(" ")
+        requestType = fields[0]
+        requestedFileName = fields[1]
+
         isBadReq = False
-        requestedFileName = ""        
-        urlStartPos = clientRequestStr.find("GET /")
-        if urlStartPos != -1:
-            urlEndPos = clientRequestStr.find("HTTP", urlStartPos)
-            if urlEndPos != -1:
-                requestedFileName = clientRequestStr[urlStartPos + 5:urlEndPos - 1]
-        if "favicon.ico" == requestedFileName or "" == requestedFileName:
+        if requestType != "GET" or "favicon.ico" == requestedFileName or "" == requestedFileName:
             isBadReq == True
         logging.getLogger('ThreadingHTTPServer').info("clientRequest file is: " + requestedFileName)
 
         if isBadReq == True:
-            self.socket.send("""HTTP/1.1 400 Bad Request
-                            Content-Type: text/html
-
-                            <html><body>Hello World</body></html>
-                            """)
+            # Bad Request
+            response = "HTTP/1.1 400 Bad Request\n\n<html><body><h1>400 Bad Request</h1></body></html>"
+            self.socket.send(response.encode())
             self.socket.close()
         else:
-            if os.path.isfile('./' + requestedFileName):
-                f = open(requestedFileName, "rb")
-                l = f.read(1024)
-                self.socket.send("""HTTP/1.1 200 OK
-                                Content-Type: text/html
+            # file found
+            if requestedFileName == "/":
+                requestedFileName = "/index.html"
 
-                                <html><body>Hello World</body></html>
-                                """)
-                self.socket.send(l)
+            if os.path.isfile('.' + requestedFileName):
+
+                # Get the last modified time, 
+                lastModifiedTime = os.path.getmtime('./' + requestedFileName)
+                lastModifiedTimeStr = datetime.fromtimestamp(lastModifiedTime).strftime('%a, %d %b %Y %H:%M:%S GMT')
+                logging.getLogger('ThreadingHTTPServer').info("lastModifiedTime is: " + lastModifiedTimeStr)
+                lastModifiedTimeHeadStr = "Last-Modified: " + lastModifiedTimeStr
+
+                # Get the file length
+                contentLength = os.path.getsize('./' + requestedFileName)
+                contentLengthStr = "Content-Length: " + str(contentLength)
+
+                # Get the file type
+                imageFileType = ["jpg","png","gif"]
+                # textFileType = ["txt",]
+                fileType = requestedFileName.split(".")[-1]
+                fileTypeStr = "Content-Type: text/html"
+                if fileType in imageFileType:
+                    fileTypeStr = "Content-Type: image/" + fileType
+                    logging.getLogger('ThreadingHTTPServer').info(fileTypeStr)
+
+                headerStr = ""
+                contentStr = ""
+                if fileTypeStr == "Content-Type: text/html":
+                    f = open('.' + requestedFileName)
+                    headerStr = """HTTP/1.1 200 OK\r\n""" + \
+                                lastModifiedTimeHeadStr + """\r\n""" + \
+                                contentLengthStr + """\r\n""" + \
+                                fileTypeStr + """\r\n\r\n"""  
+                    contentStr = f.read() 
+                    f.close()
+
+                    self.socket.send(headerStr.encode())
+                    self.socket.send(contentStr.encode())
+                else:
+                    f = open('.' + requestedFileName, "rb") # read image in binary 
+                    headerStr = """HTTP/1.1 200 OK\r\n""" + \
+                                lastModifiedTimeHeadStr + """\r\n""" + \
+                                """Accept-Ranges: bytes\r\n""" + \
+                                contentLengthStr + """\r\n""" + \
+                                fileTypeStr + """\r\n\r\n"""     
+                    contentStr = f.read()
+                    f.close()
+
+                    self.socket.send(headerStr.encode())
+                    self.socket.send(contentStr)
+                logging.getLogger('ThreadingHTTPServer').info("headerStr:" + headerStr)
+                
+                # Example
+                # HTTP/1.1 200 OK\r\n
+                # Date: Sun, 26 Sep 2010 20:09:20 GMT\r\n
+                # Server: Apache/2.0.52 (CentOS)\r\n
+                # Last-Modified: Tue, 30 Oct 2007 17:00:02 GMT\r\n
+                # ETag: "17dc6-a5c-bf716880"\r\n
+                # Accept-Ranges: bytes\r\n
+                # Content-Length: 2652\r\n
+                # Keep-Alive: timeout=10, max=100\r\n
+                # Connection: Keep-Alive\r\n
+                # Content-Type: text/html;
+
+                # headers = {"If-Modified-Since ": timestamp}
+
                 self.socket.close()
             else:
-                self.socket.send("""HTTP/1.1 404 File Not Found
-                                Content-Type: text/html
-
-                                <html><body>404 File Not Found</body></html>
-                                """)
+                # File not found
+                response = "HTTP/1.1 404 Not Found\n\n<html><body><h1>404 File Not Found</h1></body></html>"
+                self.socket.send(response.encode())
                 self.socket.close()
 
         logging.getLogger('ThreadingHTTPServer').info("Disconnected to client ..., killing thread for: " +self.ip+":"+str(self.port)) 
@@ -96,6 +147,8 @@ def run(port=80):
 
         for t in threads:
             t.join()
+        
+        serverSocket.close()
 
         logging.getLogger('ThreadingHTTPServer').info('Stopping ...\n')
 
